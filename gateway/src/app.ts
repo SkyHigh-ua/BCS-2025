ded330bd9eaa67c32eecf68d7ad5c9fc0aa00a79
Illia Rybalka <rybalka.illya@gmail.com>
2025-04-27 15:40:33 +0300

    updared services url

--- gateway/src/app.ts
+++ gateway/src/app.ts
@@ -7,24 +7,50 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   logger.info(`${req.method} ${req.url}`);
   next();
 });
 
-app.use("/api/auth", createProxyMiddleware("http://auth-service:5001"));
-app.use("/api/user", createProxyMiddleware("http://user-service:5002"));
-app.use("/api/rbac", createProxyMiddleware("http://rbac-service:5003"));
-app.use("/api/sites", createProxyMiddleware("http://site-service:5004"));
-app.use("/api/plugins", createProxyMiddleware("http://plugin-service:5005"));
 app.use(
-  "/api/modules",
-  createProxyMiddleware("http://module-controller-service:5010")
+  "/api/user",
+  createProxyMiddleware(
+    process.env.USER_SERVICE_URL || "http://user-service.local:5002"
+  )
 );
 app.use(
-  "/api/scheduler",
-  createProxyMiddleware("http://scheduler-service:5011")
+  "/api/rbac",
+  createProxyMiddleware(
+    process.env.RBAC_SERVICE_URL || "http://rbac-service.local:5003"
+  )
+);
+app.use(
+  "/api/sites",
+  createProxyMiddleware(
+    process.env.SITE_SERVICE_URL || "http://site-service.local:5004"
+  )
+);
+app.use(
+  "/api/plugins",
+  createProxyMiddleware(
+    process.env.PLUGIN_SERVICE_URL || "http://plugin-service.local:5005"
+  )
+);
+app.use(
+  "/api/modules",
+  createProxyMiddleware(
+    process.env.MODULE_SERVICE_URL ||
+      "http://module-controller-service.local:5010"
+  )
+);
+app.use(
+  "/api/auth",
+  createProxyMiddleware(
+    process.env.AUTH_SERVICE_URL || "http://auth-service.local:5001"
+  )
 );
 
 app.use((err: any, req: any, res: any, next: any) => {
