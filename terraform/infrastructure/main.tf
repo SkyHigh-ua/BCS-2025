935f1a0eafddbe6c2e1e383798e102de8907317d
Illia Rybalka <rybalka.illya@gmail.com>
2025-05-11 18:28:25 +0300

    added iam role for the ecs

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -50,6 +50,33 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+resource "aws_iam_policy" "secrets_access" {
+  name        = "ECSSecretsAccessPolicy"
+  description = "Allow ECS tasks to access secrets in Secrets Manager"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "secretsmanager:GetSecretValue",
+        ]
+        Effect = "Allow"
+        Resource = [
+          aws_secretsmanager_secret.jwt_secret.arn,
+          aws_secretsmanager_secret.db_connection.arn,
+          aws_secretsmanager_secret.db_password.arn
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_secrets_policy" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = aws_iam_policy.secrets_access.arn
+}
+
 resource "aws_secretsmanager_secret" "jwt_secret" {
   name        = "jwt-secret"
   description = "JWT secret for uptime monitoring services"
